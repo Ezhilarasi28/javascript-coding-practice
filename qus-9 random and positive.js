187623d189153f7453fba23364039003faea60f6
Ezhilarasi28 <128096795+Ezhilarasi28@users.noreply.github.com>
2024-04-02 11:22:20 +0200

    Add files via upload

---
+++ qus-9 random and positive.js
@@ -0,0 +1,46 @@
+//genereating random number
+/*const num1 = Math.random()
+console.log(num1)
+
+//positive,negative and zero*/
+
+/*let i =-100;
+if(i>0)
+{
+console.log("positive")
+}
+else if(i<0)
+{
+    console.log("negative")
+}
+else
+{
+    console.log("zero")
+}*/
+
+//method 2
+ /*let j =0;
+ if(j>0)
+ {
+    console.log("positive")
+ }
+ else if(j==0)
+ {
+    console.log("zero")
+ }
+ else 
+{
+    console.log("negative")
+}*/
+
+//find odd or even
+
+let k=10;
+if(k % 2== 0)
+{
+    console.log("even")
+}
+else
+{
+    console.log("odd")
+}
