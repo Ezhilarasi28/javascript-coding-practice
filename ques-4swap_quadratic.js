299b0cd7dc483a632b103b8d888b8afd832b1c9c
Ezhilarasi28 <zeroworld.369@gmail.com>
2024-04-02 11:42:03 +0200

    sloved

---
+++ ques-4swap_quadratic.js
@@ -0,0 +1,36 @@
+//swap method with third variable(temp)
+/*let num1 = 20;
+let num2 = 30;
+let temp;
+temp = num1,
+num1= num2,
+num2= temp
+console.log(num1,num2);*/
+
+
+//swap method without third variable
+
+/*let num1 =20;
+let num2 =30;
+num1 = num1 + num2;
+num2 = num1 - num2;
+num1 = num1 - num2;
+console.log(num1,num2)*/
+
+//swap method 
+
+/*let num1 =8
+let num2 =9
+[num1 , num2] = [num2 , num1];
+console.log(num1)
+console.log(num2)*/
+
+let temp;
+let num1 = 10;
+let num2 = 20;
+ temp= num1;
+ num1=num2;
+ num2=temp;
+ console.log("enter the first value:" ,num1)
+ console.log(" enter the second value:",num2)
+
---
+++ question-7 kilo to mile and celcius to faher.js
@@ -0,0 +1,12 @@
+//Kilometer to miles
+
+
+/*let km=10;
+const miles= (km * 0.62137)
+console.log(miles)*/
+
+//celcius to faherenheit
+
+const celcius = 40;
+const faherenheit = ((celcius * 1.8 )+32)
+console.log(faherenheit)
